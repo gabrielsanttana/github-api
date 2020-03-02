c146c5b51830b3fab991205860dc5ad875cd8ec3
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-03-01 22:42:10 -0300

    Initial commit

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {FaGithubAlt, FaPlus} from 'react-icons/fa';
+
+import {Container, Form, SubmitButton} from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input
+          type='text'
+          placeholder='Adicionar repositório'
+        />
+
+        <SubmitButton>
+          +
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 700px;
+  margin: 80px auto;
+  padding: 30px;
+  border-radius: 4px;
+  background-color: #fff;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 14px;
+  }
+`;
+
+export const SubmitButton = styled.button`
+  background-color: #7159c1;
+  margin-left: 10px;
+  padding: 0 15px;
+  border: 0;
+  border-radius: 4px;
+  color: #fff;
+  font-size: 25px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center
+`;
